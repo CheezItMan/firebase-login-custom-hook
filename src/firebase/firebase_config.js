d4b1d9cba1c312fd383af121895b63aa0747cb70
Chris McAnally <chris@adadevelopersacademy.org>
2021-07-20 12:30:12 -0700

    add firebase config

---
+++ src/firebase/firebase_config.js
@@ -0,0 +1,14 @@
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBBJCsqAUjNkDErnifkxTNXyFV2hbycfpI",
+  authDomain: "fir-auth-tic-tac-toe.firebaseapp.com",
+  projectId: "fir-auth-tic-tac-toe",
+  storageBucket: "fir-auth-tic-tac-toe.appspot.com",
+  messagingSenderId: "1033276194699",
+  appId: "1:1033276194699:web:a9c71f321bd60f006d798e",
+  measurementId: "G-52KQC7RXQJ"
+};
+
+export default firebaseConfig;
