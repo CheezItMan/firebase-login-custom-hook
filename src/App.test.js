3d071ca884fbc0241a0b656a6d3544a860b05aa4
Chris McAnally <chris@adadevelopersacademy.org>
2021-07-22 13:17:56 -0700

    Tests for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,45 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Component', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+  test('renders Login with Google Button', async () => {
+    render(<App />);
+    expect(await screen.findByText(/Login/i)).toBeInTheDocument();
+  });
+
+  test('you can click on the Login with Google button and get the profile', async () => {
+    // Arrange
+    render(<App />);
+    let loginButton = await screen.findByText(/Login/i);
+    expect(loginButton).toBeInTheDocument();
+
+    // Act
+    fireEvent.click(loginButton, { target: { value: 'Login with Google' } });
+
+    const profile = await screen.findByText(/Spock/);
+    expect(profile).toBeInTheDocument();
+  });
+
+  test('you can click on the Logout! with Google button and get the profile', async () => {
+    // Arrange
+    render(<App />);
+    let loginButton = await screen.findByText(/Login/i);
+    expect(loginButton).toBeInTheDocument();
+
+    // Act
+    fireEvent.click(loginButton, { target: { value: 'Login with Google' } });
+
+    const profile = await screen.findByText(/Spock/);
+    expect(profile).toBeInTheDocument();
+
+    const logoutButton = await screen.findByText(/Logout/i);
+    // Act
+    fireEvent.click(logoutButton, { target: { value: 'Logout' } });
+    loginButton = await screen.findByText(/Login/i);
+    expect(loginButton).toBeInTheDocument();
+    expect(profile).not.toBeInTheDocument();
+  });
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,59 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import firebase from 'firebase/app';
+
+
+jest.mock('firebase/app', () => {
+  return {
+    status: 'LoggedOut',
+    initializeApp: function () {
+      this.auth.GoogleAuthProvider = jest.fn();
+      return jest.fn()();
+    },
+    auth: function () {
+      const that = this;
+      return {
+        signOut: function () {
+          this.status = 'LoggedOut';
+          return new Promise((resolve, reject) => {
+            resolve({});
+          });
+        },
+        getRedirectResult: function () {
+          return new Promise((resolve, reject) => {
+            if (that.status === 'LoggedOut') {
+              resolve({});
+              return;
+            }
+            resolve({
+              credential: '12345',
+              user: {
+                id: 1,
+                displayName: 'Spock',
+                photoURL: 'https://placekitten.com/100/150',
+              },
+            })
+          });
+        },
+        signInWithRedirect: function (provider) {
+          this.status = 'LoggedIn';
+          return new Promise((resolve, reject) => {
+            resolve({
+              credential: '12345',
+              user: {
+                id: 1,
+                displayName: 'Spock',
+                photoURL: 'https://placekitten.com/100/150',
+              },
+            });
+          });
+        },
+      }
+    },
+  }
+}); // End Mock return
+    
+const AuthProvider = function AuthProvider () { };
+
+firebase.auth.GoogleAuthProvider = jest.fn(() => { return AuthProvider });
